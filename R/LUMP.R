d957559a9e15cd9b60fd34508ba49a4ac5d9ad7a
Zhichao Wu <yeswzc@users.noreply.github.com>
2020-05-04 16:33:21 -0400

    Update LUMP.R

--- R/LUMP.R
+++ R/LUMP.R
@@ -1,9 +1,14 @@
-#probes used to estimate purity
-load(lump.cpg.list.rda)
-
-lump = function(betas){
-#betas: matrix/data.frame wiith columns as probe IDs and row as samples
+load("../lump.cpg.list.rda") #probes used to estimate purity
+lump = function(betas, sample_in_row = "Y"){
+  #betas: matrix/data.frame if 
+  #columns as probe IDs and row as samples: sample_in_row = Y
+  #else sample_in_row = "N
+  if(sample_in_row == "Y"){
     betas = betas[,colnames(betas) %in% lump_cpg]
-    lump_purity = apply(res_betas, MARGIN = 1, function(x){ LUMP = min(1, mean(x)/0.85); LUMP}
-    return(lump_purity)
+    lump_purity = apply(betas, MARGIN = 1, function(x){ LUMP = min(1, mean(x)/0.85); LUMP})
+  }else{
+    betas = betas[rownames(betas) %in% lump_cpg,]
+    lump_purity = apply(betas, MARGIN = 2, function(x){ LUMP = min(1, mean(x)/0.85); LUMP})
+  }
+  return(lump_purity)
 }
