99107a3f96b37dbb38b2d2b81936d0da15bcd430
Zhichao Wu <yeswzc@users.noreply.github.com>
2020-05-05 15:53:11 -0400

    Update LUMP.R

--- R/LUMP.R
+++ R/LUMP.R
@@ -4,10 +4,10 @@ lump = function(betas, sample_in_row = "Y"){
   #columns as probe IDs and row as samples: sample_in_row = Y
   #else sample_in_row = "N
   if(sample_in_row == "Y"){
-    betas = betas[,colnames(betas) %in% lump_cpg]
+    betas = betas[,colnames(betas) %in% lump_cpg, drop=F]
     lump_purity = apply(betas, MARGIN = 1, function(x){ LUMP = min(1, mean(x)/0.85); LUMP})
   }else{
-    betas = betas[rownames(betas) %in% lump_cpg,]
+    betas = betas[rownames(betas) %in% lump_cpg,, drop=F]
     lump_purity = apply(betas, MARGIN = 2, function(x){ LUMP = min(1, mean(x)/0.85); LUMP})
   }
   return(lump_purity)
