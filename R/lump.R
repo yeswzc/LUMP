8e727df8d62a7bb5f4a6eb2dc859e95bb48ac12c
Zhichao Wu <yeswzc@users.noreply.github.com>
2020-05-18 10:56:33 -0400

    Update lump.R

--- R/lump.R
+++ R/lump.R
@@ -1,5 +1,5 @@
 #' @export
-lump = function(betas, sample_in_row = "Y"){
+lump = function(betas, sample_in_row = "N"){
       #betas: matrix/data.frame if 
       #columns as probe IDs and row as samples: sample_in_row = Y
       #else sample_in_row = "N
